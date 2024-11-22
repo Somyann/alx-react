94e25e91dc6d4f6b1509d691393108551bcfb12d
Somyann <somiaprecious91@gmail.com>
2024-11-22 17:52:45 +0100

    nested

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,7 +1,18 @@
+import { fromJS } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  const immutableObject = Map(object);
+  const immutableObject = fromJS(object);
 
   return immutableObject.getIn(array);
 }
+
+// Example Usage
+const obj = {
+  name: {
+    first: "Guillaume",
+    last: "Salva",
+  },
+};
+
+console.log(accessImmutableObject(obj, ['name', 'first']));
 
