0462d3339299245132c127f4ebb2ef1648d9de79
Somyann <somiaprecious91@gmail.com>
2024-11-29 15:52:44 +0100

    react redux

--- 0x07-react_redux_action_creator_normalizr/jest.config.mjs
+++
@@ -1,4 +0,0 @@
-export const transform = {
-  '^.+\\.jsx?$': 'babel-jest', // or 'ts-jest' if you're using TypeScript
-};
-export const extensionsToTreatAsEsm = ['.jsx', '.js'];
--- 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.test.js
+++ 0x07-react_redux_action_creator_normalizr/task_1/dashboard/src/schema/notifications.test.js
@@ -1,31 +1,64 @@
-import { getDataById } from './notifications';
+const { getDataById } = require('./notifications');
+
+const { getDataById } = require ('./notifications');
+require 
 
 describe('getDataById', () => {
   it('should return the correct data for ID 5debd764a7c57c7839d722e9', () => {
-    const inputId = '5debd764a7c57c7839d722e9';
 
-    
-    const expectedData = [
-      {
-        guid: '2d8e40be-1c78-4de0-afc9-fcc147afd4d2',
+      const expectedData = [ {
+        guid: "2d8e40be-1c78-4de0-afc9-fcc147afd4d2",
         isRead: true,
-        type: 'urgent',
+        type: "urgent",
         value:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.',
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
       },
       {
-        guid: '280913fe-38dd-4abd-8ab6-acdb4105f922',
+        guid: "280913fe-38dd-4abd-8ab6-acdb4105f922",
         isRead: false,
-        type: 'urgent',
-        value:
-          'Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed',
+        type: "urgent",
+        value: "Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed",
       },
     ];
-
-    // Call the function and store the result
-    const result = getDataById(inputId);
-
-    // Compare the result with the expected data
-    expect(result).toEqual(expectedData);
+      const result = getDataById("5debd764a7c57c7839d722e9");
+      expect(result).toEqual(expect.arrayContaining(expectedData));
+    });
   });
-});
+  describe('getDataById', () => {
+    it('should return the correct data for ID 5debd764a7c57c7839d722e9', () => {
+      const expectedData = [
+        {
+          guid: "2d8e40be-1c78-4de0-afc9-fcc147afd4d2",
+          isRead: true,
+          type: "urgent",
+          value: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.",
+        },
+        {
+          guid: "280913fe-38dd-4abd-8ab6-acdb4105f922",
+          isRead: false,
+          type: "urgent",
+          value: "Non diam phasellus vestibulum lorem sed risus ultricies. Tellus mauris a diam maecenas sed",
+        },
+      ];
+      const result = getDataById("5debd764a7c57c7839d722e9");
+      expect(result).toEqual(expect.arrayContaining(expectedData));
+    });
+
+    it('should return an empty array for an unknown ID', () => {
+      const result = getDataById("unknown_id");
+      expect(result).toEqual([]);
+    });
+
+    it('should return the correct data for ID 5debd764a7c57c7839d722e8', () => {
+      const expectedData = [
+        {
+          guid: "1d8e40be-1c78-4de0-afc9-fcc147afd4d1",
+          isRead: false,
+          type: "default",
+          value: "Hello world",
+        },
+      ];
+      const result = getDataById("5debd764a7c57c7839d722e8");
+      expect(result).toEqual(expect.arrayContaining(expectedData));
+    });
+  });
